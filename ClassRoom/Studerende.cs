e2a160f33e15afd83788a7562a04d968a3f9d2ec
AsianDave94 <Davidtjmalmberg@gmail.com>
2018-02-02 16:07:33 +0100

    skelet

---
+++ ClassRoom/Studerende.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+   public class Studerende
+    {
+        public string Navn
+        {
+            get;set;
+        }
+        public int Fødselsmåned
+        {
+            get; set;
+        }
+        public int Fødselsdag
+        {
+            get;set;
+        }
+        public Studerende(string int int)
+        {
+
+        }
+    }
+}
